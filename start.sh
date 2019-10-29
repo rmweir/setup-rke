9ce983a96312b1a7ce0dce5ac4dc707020d946c4
rmweir <ricardo.weir1@gmail.com>
2019-10-29 12:19:51 -0700

    detail cluster.yml and turn of strict checking

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 IP=$1
 USER=$2
-
-ssh -vvv -o StrictHostKeyChecking=no -l root $IP "git clone https://github.com/rancher/install-docker.git &&
+KEYPATH=$3
+ssh -i $KEYPATH -vvv -o StrictHostKeyChecking=no -l $USER $IP "git clone https://github.com/rancher/install-docker.git &&
 cd install-docker &&
 chmod +x 17.03.sh &&
 ./17.03.sh &&
 sudo usermod -aG docker $USER &&
 sudo iptables -A INPUT -p tcp --dport 6443 -j ACCEPT"
-rke up
